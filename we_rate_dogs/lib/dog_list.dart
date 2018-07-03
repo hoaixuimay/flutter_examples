358a5d8db1373335fee402bea8bad8180ed0930a
Hoai Pham <hoaixuimay@gmail.com>
2018-07-03 18:30:11 +0700

    update

---
+++ we_rate_dogs/lib/dog_list.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:we_rate_dogs/dog_model.dart';
+import 'package:we_rate_dogs/dog_card.dart';
+
+class DogList extends StatelessWidget {
+  final List<Dog> doggos;
+
+  DogList(this.doggos);
+
+  ListView _buildList(context){
+    return new ListView.builder(
+      itemCount: doggos.length,
+      itemBuilder: (context, int){
+        return new DogCard(doggos[int]);
+      }
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _buildList(context);
+  }
+
+}
---
+++ we_rate_dogs/lib/new_dog_form.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:we_rate_dogs/dog_model.dart';
+
+class AddDogFormPage extends StatefulWidget {
+  @override
+  State<AddDogFormPage> createState() {
+    return new _AddDogFormState();
+  }
+
+}
+
+class _AddDogFormState extends State<AddDogFormPage>{
+
+  TextEditingController nameController = new TextEditingController();
+  TextEditingController locationController = new TextEditingController();
+  TextEditingController descriptionController = new TextEditingController();
+
+  void submitPup(context){
+    if(nameController.text.isEmpty){
+      Scaffold.of(context).showSnackBar(
+        new SnackBar(
+          backgroundColor: Colors.redAccent,
+            content: new Text("Pups need names!")
+        )
+      );
+    } else {
+      var newDog = new Dog(nameController.text, locationController.text, descriptionController.text);
+      Navigator.of(context).pop(newDog);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Add a new Dog'),
+        backgroundColor: Colors.black87,
+      ),
+      body: new Container(
+        color: Colors.black54,
+        child: new Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 8.0,
+              horizontal: 32.0
+            ),
+          child: new Column(
+            children: <Widget>[
+              new Padding(
+                  padding: const EdgeInsets.only(bottom: 8.0),
+                child: new TextField(
+                  controller: nameController,
+                  decoration: new InputDecoration(
+                    labelText: 'Name the Pup'
+                  ),
+                ),
+              ),
+              new Padding(
+                  padding: const EdgeInsets.only(bottom: 8.0),
+                child: new TextField(
+                  controller: locationController,
+                  decoration: new InputDecoration(
+                    labelText: 'Pups location'
+                  ),
+                )
+              ),
+              new Padding(
+                  padding: const EdgeInsets.only(bottom: 8.0),
+                child: new TextField(
+                  controller: descriptionController,
+                  decoration: new InputDecoration(
+                    labelText: 'All about the pup'
+                  ),
+                ),
+              ),
+              new Padding(
+                  padding: const EdgeInsets.all(16.0),
+                child: new Builder(
+                  builder: (context) {
+                    return RaisedButton(
+                      color: Colors.indigoAccent,
+                      child: new Text('Submit Pup'),
+                      onPressed: () => submitPup(context),
+                    );
+                  }
+                  ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
